456fcdc33d21070c22a4df8b480e088e640899d8
Sebastian Mayr <github@smayr.name>
2015-12-25 18:26:24 +0100

    Update test file for new path format

--- test/test.js
+++ test/test.js
@@ -4,5 +4,5 @@ const transformer = require("..");
 transformer.generate(
   fs.readFileSync(__dirname + "/cases/URL.idl", { encoding: "utf-8" }),
   __dirname + "/output/",
-  "../implementations/"
+  "./test/implementations/"
 );
