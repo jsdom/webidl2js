jsdom/webidl2js

commit c2214f1eaaeb44564bd53ce31fc1f1eb741ef5a8

Do inheritance a bit more correctly
Closes #27.

lib/constructs/interface.js
@@ -115,8 +115,8 @@ Interface.prototype.generateConstructor = function () {
   }
 
   if (this.idl.inheritance) {
-    this.str += `${this.name}.prototype = Object.create(${this.idl.inheritance}.interface.prototype);
-${this.name}.prototype.constructor = ${this.name};\n`;
+    this.str += `Object.setPrototypeOf(${this.name}.prototype, ${this.idl.inheritance}.interface.prototype);
+Object.setPrototypeOf(${this.name}, ${this.idl.inheritance}.name);\n`;
   }
 };
 

commit 0dd39ee86a331bf78b34987ec2c2cec5604127ca

Fix inheritance bug
This was introduced in c2214f1eaaeb44564bd53ce31fc1f1eb741ef5a8.

lib/constructs/interface.js
@@ -118,7 +118,7 @@ Interface.prototype.generateConstructor = function () {
 
   if (this.idl.inheritance) {
     this.str += `Object.setPrototypeOf(${this.name}.prototype, ${this.idl.inheritance}.interface.prototype);
-Object.setPrototypeOf(${this.name}, ${this.idl.inheritance}.name);\n`;
+Object.setPrototypeOf(${this.name}, ${this.idl.inheritance}.interface);\n`;
   }
 };
 