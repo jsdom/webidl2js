0c7e4a247120dd87d0f86fc3a76694307a5f0a74
Domenic Denicola <d@domenic.me>
2017-04-29 10:47:01 -0400

    Make the tests a bit more useful

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const Transformer = require("..");
 
 const transformer = new Transformer()
-  .addSource(__dirname + "/cases/URL.idl", __dirname + "/implementations/")
+  .addSource(__dirname + "/cases/", __dirname + "/implementations/")
   .generate(__dirname + "/output/")
   .catch((err) => {
     console.error(err);
