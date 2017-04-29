jsdom/webidl2js

commit 4dd82b0af691ba0c345f1dc24c44f33be7d4b7f0

Don't emit useless code for zero-arg operations/constructors
Closes #34.

lib/parameters.js
@@ -93,23 +93,24 @@ module.exports.generateOverloadConversions = function (overloads, customTypes) {
 
   const typeConversions = Overloads.proveSimiliarity(overloads);
 
-  str += `
+  const isAlwaysZeroArgs = !isVariadic && maxArguments === 0;
+  if (!isAlwaysZeroArgs) {
+    const extraClause = !isVariadic ? ` && i < ${maxArguments}` : ``;
+    str += `\n
   const args = [];
-  for (let i = 0; i < arguments.length`;
-  if (!isVariadic) {
-    str += ` && i < ${maxArguments}`;
-  }
-  str += `; ++i) {
+  for (let i = 0; i < arguments.length${extraClause}; ++i) {
     args[i] = utils.tryImplForWrapper(arguments[i]);
   }`;
-  for (let i = 0; i < typeConversions.length; ++i) {
-    if (typeConversions[i] === null) {
-      continue;
-    }
 
-    const conv = module.exports.generateVarConversion(`args[${i}]`, typeConversions[i], maxConstructor.operation.arguments[i].extAttrs, customTypes);
-    Object.assign(requires, conv.requires);
-    str += conv.body;
+    for (let i = 0; i < typeConversions.length; ++i) {
+      if (typeConversions[i] === null) {
+        continue;
+      }
+
+      const conv = module.exports.generateVarConversion(`args[${i}]`, typeConversions[i], maxConstructor.operation.arguments[i].extAttrs, customTypes);
+      Object.assign(requires, conv.requires);
+      str += conv.body;
+    }
   }
   return {
     requires,

commit 501309118d18d21cd6326f7c9e20ccd3a0cc1e00

Fix bug in zero-args calls to the impl
This was introduced in 4dd82b0af691ba0c345f1dc24c44f33be7d4b7f0.

lib/constructs/operation.js
@@ -53,22 +53,23 @@ Operation.prototype.generate = function () {
   const callOn = this.idl.static ? "Impl" : "this[impl]";
 
   const parameterConversions = Parameters.generateOverloadConversions(overloads, this.obj.opts.customTypes);
+  const argsSpread = parameterConversions.hasArgs ? "...args" : "";
   Object.assign(requires, parameterConversions.requires);
   str += parameterConversions.body;
   if (!this.idl.stringifier && overloads.every((overload) => conversions[overload.operation.idlType.idlType])) {
     str += `
-  return ${callOn}.${name}.apply(${callOn}, args);
+  return ${callOn}.${name}(${argsSpread});
 };\n`;
   } else {
     str += `
-  return utils.tryWrapperForImpl(${callOn}.${name}.apply(${callOn}, args));
+  return utils.tryWrapperForImpl(${callOn}.${name}(${argsSpread}));
 };\n`;
   }
   if (this.idl.stringifier && name !== "toString") {
     str += `\n${this.name}.prototype.toString(${minConstructor.nameList.join(", ")}) {`;
     str += parameterConversions.body;
     str += `
-  return ${callOn}.${name}.apply(${callOn}, args);
+  return ${callOn}.${name}.apply(${argsSpread});
 };\n`;
   }
 

lib/parameters.js
@@ -114,6 +114,7 @@ module.exports.generateOverloadConversions = function (overloads, customTypes) {
   }
   return {
     requires,
-    body: str
+    body: str,
+    hasArgs: !isAlwaysZeroArgs
   };
 };