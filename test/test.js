0f89e7ba5d505c2592169eb239ccba36f9af4ff3
Domenic Denicola <d@domenic.me>
2017-05-06 16:09:03 -0400

    Add Jest-based snapshot testing and Travis
    Previously we were just kind of running test/test.js as needed and hoping for the best. With this in place we have an easier way of checking for regressions.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,27 @@
+"use strict";
+
 const fs = require("fs");
+const path = require("path");
 const Transformer = require("..");
 
-const transformer = new Transformer()
-  .addSource(__dirname + "/cases/", __dirname + "/implementations/")
-  .generate(__dirname + "/output/")
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
+const casesDir = path.resolve(__dirname, "cases");
+const implsDir = path.resolve(__dirname, "implementations");
+const outputDir = path.resolve(__dirname, "output");
+
+beforeAll(() => {
+  const transformer = new Transformer();
+  transformer.addSource(casesDir, implsDir);
+
+  return transformer.generate(outputDir);
+});
+
+const idlFiles = fs.readdirSync(casesDir);
+
+for (const idlFile of idlFiles) {
+  test(idlFile, () => {
+    const outputFile = path.resolve(outputDir, path.basename(idlFile, ".idl") + ".js");
+    const output = fs.readFileSync(outputFile, { encoding: "utf-8" });
+
+    expect(output).toMatchSnapshot();
   });
+}
