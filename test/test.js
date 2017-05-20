030e0f3dd5f6cb3f8c88dd2d38117890def7a36d
Timothy Gu <timothygu99@gmail.com>
2017-05-20 10:13:28 -0700

    Add test for utils.js copying

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const Transformer = require("..");
 
+const rootDir = path.resolve(__dirname, "..");
 const casesDir = path.resolve(__dirname, "cases");
 const implsDir = path.resolve(__dirname, "implementations");
 const outputDir = path.resolve(__dirname, "output");
@@ -25,3 +26,9 @@ for (const idlFile of idlFiles) {
     expect(output).toMatchSnapshot();
   });
 }
+
+test("utils.js", () => {
+  const input = fs.readFileSync(path.resolve(rootDir, "lib/output/utils.js"), { encoding: "utf-8" });
+  const output = fs.readFileSync(path.resolve(outputDir, "utils.js"), { encoding: "utf-8" });
+  expect(output).toBe(input);
+});
