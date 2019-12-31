5d69cdb1634f41830cf1e7cb80f0dedf03c54813
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-12-31 13:30:00 +0100

    chore(eslint): Use `overrides` to set correct `env` for tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,52 +1,29 @@
-// eslint-disable-next-line spaced-comment
-/* eslint-env node, jest */
-/* global BigInt */
-
 "use strict";
 
 const utils = require("../lib/output/utils");
 
 describe("utils.js", () => {
   describe("isObject", () => {
-    const BIG_INT_PLACEHOLDER = "BigInt placeholder";
-
-    /** @type {Array<string | number | bigint | boolean | symbol | null | undefined>} */
     const primitives = [
       123,
       "string",
       Symbol.iterator,
       true,
-      BIG_INT_PLACEHOLDER,
       null,
       undefined
     ];
 
-    // eslint-disable-next-line new-cap, valid-typeof
-    if (typeof BigInt === "function" && typeof BigInt("123") === "bigint") {
-      primitives.splice(
-        primitives.indexOf(BIG_INT_PLACEHOLDER),
-        1,
-        // eslint-disable-next-line new-cap
-        BigInt("123")
-      );
-    }
-
     for (const primitive of primitives) {
-      /** @type {"string" | "number" | "bigint" | "boolean" | "symbol" | "null" | "undefined"} */
-      let type = primitive === null ? "null" : typeof primitive;
-
-      let testPrimitive = test;
-
-      if (primitive === BIG_INT_PLACEHOLDER) {
-        // BigInt doesn't exist on Node 8, so we have to assign this manually:
-        type = "bigint";
-        testPrimitive = test.skip;
-      }
-
-      testPrimitive(type, () => {
+      test(primitive === null ? "null" : typeof primitive, () => {
         expect(utils.isObject(primitive)).toBe(false);
       });
     }
+
+    const testBigInt = typeof BigInt === "function" && typeof BigInt("123") === "bigint" ? test : test.skip;
+
+    testBigInt("bigint", () => {
+      expect(utils.isObject(BigInt(123))).toBe(false);
+    });
 
     test("object", () => {
       expect(utils.isObject({})).toBe(true);
