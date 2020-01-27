2aecfb6816e34c614b390acb2ac8eb49509c5c21
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2020-01-27 06:58:15 -0800

    Fix default processor signature
    Fixes a bug that sneaked in fda810e and that was uncaught by the test suite.

--- test/test.js
+++ test/test.js
@@ -9,43 +9,63 @@ const casesDir = path.resolve(__dirname, "cases");
 const implsDir = path.resolve(__dirname, "implementations");
 const outputDir = path.resolve(__dirname, "output");
 
-beforeAll(() => {
-  const transformer = new Transformer({
-    processCEReactions(code) {
-      const ceReactions = this.addImport("../CEReactions");
-
-      return `
-        ${ceReactions}.preSteps(globalObject);
-        try {
-          ${code}
-        } finally {
-          ${ceReactions}.postSteps(globalObject);
-        }
-      `;
-    },
-    processHTMLConstructor() {
-      const htmlConstructor = this.addImport("../HTMLConstructor", "HTMLConstructor");
-
-      return `
-        return ${htmlConstructor}(globalObject, interfaceName);
-      `;
-    }
-  });
-  transformer.addSource(casesDir, implsDir);
-
-  return transformer.generate(outputDir);
-});
-
 const idlFiles = fs.readdirSync(casesDir);
 
-for (const idlFile of idlFiles) {
-  test(idlFile, () => {
-    const outputFile = path.resolve(outputDir, path.basename(idlFile, ".webidl") + ".js");
-    const output = fs.readFileSync(outputFile, { encoding: "utf-8" });
+describe("without processors", () => {
+  beforeAll(() => {
+    const transformer = new Transformer();
+    transformer.addSource(casesDir, implsDir);
 
-    expect(output).toMatchSnapshot();
+    return transformer.generate(outputDir);
   });
-}
+
+  for (const idlFile of idlFiles) {
+    test(idlFile, () => {
+      const outputFile = path.resolve(outputDir, path.basename(idlFile, ".webidl") + ".js");
+      const output = fs.readFileSync(outputFile, { encoding: "utf-8" });
+
+      expect(output).toMatchSnapshot();
+    });
+  }
+});
+
+describe("with processors", () => {
+  beforeAll(() => {
+    const transformer = new Transformer({
+      processCEReactions(code) {
+        const ceReactions = this.addImport("../CEReactions");
+
+        return `
+          ${ceReactions}.preSteps(globalObject);
+          try {
+            ${code}
+          } finally {
+            ${ceReactions}.postSteps(globalObject);
+          }
+        `;
+      },
+      processHTMLConstructor() {
+        const htmlConstructor = this.addImport("../HTMLConstructor", "HTMLConstructor");
+
+        return `
+          return ${htmlConstructor}(globalObject, interfaceName);
+        `;
+      }
+    });
+    transformer.addSource(casesDir, implsDir);
+
+    return transformer.generate(outputDir);
+  });
+
+  for (const idlFile of idlFiles) {
+    test(idlFile, () => {
+      const outputFile = path.resolve(outputDir, path.basename(idlFile, ".webidl") + ".js");
+      const output = fs.readFileSync(outputFile, { encoding: "utf-8" });
+
+      expect(output).toMatchSnapshot();
+    });
+  }
+});
 
 test("utils.js", () => {
   const input = fs.readFileSync(path.resolve(rootDir, "lib/output/utils.js"), { encoding: "utf-8" });
