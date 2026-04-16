09ec6075a1f02b709d79c9dccc5fe4ce4aa7dab0
Mattias Buelens <649348+MattiasBuelens@users.noreply.github.com>
2026-04-16 08:53:12 +0200

    Support async sequence types

--- test/cases/SeqAndRec.webidl
+++ test/cases/SeqAndRec.webidl
@@ -6,5 +6,9 @@ interface SeqAndRec {
   undefined recordConsumer2(record<USVString, URL> rec);
   undefined sequenceConsumer(sequence<USVString> seq);
   undefined sequenceConsumer2(sequence<UnknownInterface> seq);
+  undefined asyncSequenceConsumer(async_sequence<USVString> async_seq);
+  undefined asyncSequenceConsumer2(async_sequence<UnknownInterface> async_seq);
   undefined frozenArrayConsumer(FrozenArray<double> arr);
+
+  async_sequence<double> asyncSequencePassthrough(async_sequence<double> async_seq);
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -49,4 +49,190 @@ describe("utils.js", () => {
       expect(object).not.toBeInstanceOf(realm.Object);
     });
   });
+
+  describe("convertAsyncSequence", () => {
+    const iterableFactories = [
+      [
+        "an array of values", () => {
+          return ["a", "b"];
+        }
+      ],
+
+      [
+        "an array of promises", () => {
+          return [
+            Promise.resolve("a"),
+            Promise.resolve("b")
+          ];
+        }
+      ],
+
+      [
+        "an array iterator", () => {
+          return ["a", "b"][Symbol.iterator]();
+        }
+      ],
+
+      [
+        "a Set", () => {
+          return new Set(["a", "b"]);
+        }
+      ],
+
+      [
+        "a Set iterator", () => {
+          return new Set(["a", "b"])[Symbol.iterator]();
+        }
+      ],
+
+      [
+        "a sync generator", () => {
+          function* syncGenerator() {
+            yield "a";
+            yield "b";
+          }
+
+          return syncGenerator();
+        }
+      ],
+
+      [
+        "an async generator", () => {
+          async function* asyncGenerator() {
+            yield "a";
+            yield "b";
+          }
+
+          return asyncGenerator();
+        }
+      ],
+
+      [
+        "a sync iterable of values", () => {
+          const chunks = ["a", "b"];
+          const iterator = {
+            next() {
+              return {
+                done: chunks.length === 0,
+                value: chunks.shift()
+              };
+            }
+          };
+          const iterable = {
+            [Symbol.iterator]: () => iterator
+          };
+          return iterable;
+        }
+      ],
+
+      [
+        "a sync iterable of promises", () => {
+          const chunks = ["a", "b"];
+          const iterator = {
+            next() {
+              return chunks.length === 0 ?
+                { done: true } :
+                {
+                  done: false,
+                  value: Promise.resolve(chunks.shift())
+                };
+            }
+          };
+          const iterable = {
+            [Symbol.iterator]: () => iterator
+          };
+          return iterable;
+        }
+      ],
+
+      [
+        "an async iterable", () => {
+          const chunks = ["a", "b"];
+          const asyncIterator = {
+            next() {
+              return Promise.resolve({
+                done: chunks.length === 0,
+                value: chunks.shift()
+              });
+            }
+          };
+          const asyncIterable = {
+            [Symbol.asyncIterator]: () => asyncIterator
+          };
+          return asyncIterable;
+        }
+      ]
+    ];
+
+    for (const [label, factory] of iterableFactories) {
+      test(`accepts ${label}`, async () => {
+        const iterable = factory();
+        const isAsync = label.includes("async");
+        const asyncSequence = utils.convertAsyncSequence(iterable, x => x);
+
+        expect(asyncSequence.object).toBe(iterable);
+        expect(asyncSequence.method).toBe(isAsync ? iterable[Symbol.asyncIterator] : iterable[Symbol.iterator]);
+        expect(asyncSequence.type).toBe(isAsync ? "async" : "sync");
+        expect(utils.wrapperForImpl(asyncSequence)).toBe(iterable);
+
+        const iterator = asyncSequence[Symbol.asyncIterator]();
+        await expect(iterator.next()).resolves.toEqual({ done: false, value: "a" });
+        await expect(iterator.next()).resolves.toEqual({ done: false, value: "b" });
+        await expect(iterator.next()).resolves.toEqual({ done: true, value: undefined });
+      });
+    }
+
+    const badIterables = [
+      ["null", null],
+      ["undefined", undefined],
+      ["0", 0],
+      ["NaN", NaN],
+      ["true", true],
+      ["{}", {}],
+      ["Object.create(null)", Object.create(null)],
+      ["a function", () => 42],
+      ["a symbol", Symbol("foo")],
+      ["a string", "ab"],
+      [
+        "an object with a non-callable @@iterator method", {
+          [Symbol.iterator]: 42
+        }
+      ],
+      [
+        "an object with a non-callable @@asyncIterator method", {
+          [Symbol.asyncIterator]: 42
+        }
+      ]
+    ];
+
+    for (const [label, iterable] of badIterables) {
+      test(`throws on invalid iterables; specifically ${label}`, () => {
+        expect(() => {
+          utils.convertAsyncSequence(iterable, x => x);
+        }).toThrow(TypeError);
+      });
+    }
+
+    const badIteratorMethods = [
+      [
+        "an object with an @@iterator method returning a non-object", {
+          [Symbol.iterator]: () => 42
+        }
+      ],
+      [
+        "an object with an @@asyncIterator method returning a non-object", {
+          [Symbol.asyncIterator]: () => 42
+        }
+      ]
+    ];
+
+    for (const [label, iterable] of badIteratorMethods) {
+      test(`throws when opening iterables with bad iterator methods; specifically ${label}`, () => {
+        const asyncSequence = utils.convertAsyncSequence(iterable, x => x);
+        expect(() => {
+          asyncSequence[Symbol.asyncIterator]();
+        }).toThrow(TypeError);
+      });
+    }
+  });
 });
